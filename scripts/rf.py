4c9332944b15217fedc06e8bea0884c1a43fd1ca
Anderson Frasao <anderfrasao52@gmail.com>
2024-03-03 19:31:04 -0300

    Ainda tem que testar, mas ta pesado

--- scripts/rf.py
+++ scripts/rf.py
@@ -102,11 +102,13 @@ class Main():
             print("read_data: ", e)
 
     def grid_search_classifier(self, classifier_name, classifier, param_grid, X_train, y_train, X_test, y_test):
-        grid_search = GridSearchCV(classifier, param_grid, cv=5, scoring='accuracy')
+        print('Fazendo pesquisa exaustiva sobre valores de parâmetros especificados para um estimador.')
+        grid_search = GridSearchCV(classifier, param_grid, cv=5, scoring='f1')
+        print('Ajuste com todos os conjuntos de parâmetros.')
         grid_search.fit(X_train, y_train)
 
-        # print("Melhores parâmetros para", classifier_name, ":", grid_search.best_params_)
-        # print("Melhor pontuação para", classifier_name, ":", grid_search.best_score_)
+        print("Melhores parâmetros para", classifier_name, ":", grid_search.best_params_)
+        print("Melhor pontuação para", classifier_name, ":", grid_search.best_score_)
 
         best_classifier = grid_search.best_estimator_
         best_classifier.fit(X_train, y_train)
@@ -118,14 +120,58 @@ class Main():
         X_train, X_test, y_train, y_test, X, y = self.read_data()
 
         # Essa parte:
-        print("Otimizado")
+        print('Otimizado')
+
+        # Variaveis de RandomForestClassifier:
+        # n_estimators -------------> O número de árvores na floresta.
+        # criterion ----------------> A função para medir a qualidade de uma divisão.
+        # max_depth ----------------> A profundidade máxima da árvore.
+        # min_samples_split --------> O número mínimo de amostras necessárias para dividir um nó interno.
+        # min_samples_leaf ---------> O número mínimo de amostras necessárias para estar em um nó folha.
+        # min_weight_fraction_leaf -> A fração ponderada mínima da soma total dos pesos (de todas as amostras de entrada) necessária para estar em um nó folha.
+        # max_features -------------> O número de características a considerar quando se procura o melhor split.
+        # max_leaf_nodes -----------> Cultivar árvores com max_leaf_nodes da melhor forma possível.
+        # min_impurity_decrease ----> Um nó será dividido se esta divisão induzir uma diminuição da impureza maior ou igual a este valor.
+        # bootstrap ----------------> Se as amostras bootstrap são utilizadas na construção de árvores.
+        # n_jobs -------------------> O número de trabalhos a executar em paralelo.
+        # random_state -------------> Controla a aleatoriedade do bootstrapping das amostras utilizadas na construção de árvores (se bootstrap=True) e a amostragem das características a considerar ao procurar a melhor divisão em cada nó (se max_features < n_features).
+        # class_weight -------------> Pesos associados às classes na forma {class_label: weight}.
+        # max_samples --------------> Se bootstrap for Verdadeiro, o número de amostras a retirar de X para treinar cada estimador de base.
 
         # RandomForestClassifier
         rfc_param_grid = {
-            'max_depth': [None],
-            'min_samples_leaf': [1],
-            'min_samples_split': [2],
-            'n_estimators': [50]
+            'n_estimators': [50, 75],
+            'criterion': ['gini', 'entropy', 'log_loss'],
+            'max_depth': [None, 1, 50],
+            'min_samples_split': [2, 25],
+            'min_samples_leaf': [1, 25],
+            'min_weight_fraction_leaf': [0.1, 0.25],
+            'max_features': ['sqrt', 'log2', None],
+            'max_leaf_nodes': [1, 25],
+            'min_impurity_decrease': [0.1, 0.25],
+            'bootstrap': [True, False],
+            'n_jobs': [-1],
+            'random_state': [None, 1, 25],
+            'class_weight': ['balanced', 'balanced_subsample'],
+            'max_samples': [1, 25],
+        }
+        # Esses valores são bons, mas tem poucos parametros
+        #rfc_param_grid = {
+        #    'max_depth': [None],
+        #    'min_samples_leaf': [1],
+        #    'min_samples_split': [2],
+        #    'n_estimators': [50]
+        #}
+        self.grid_search_classifier("rfc", RandomForestClassifier(), rfc_param_grid, X_train, y_train, X_test, y_test)
+        exit(1)
+        # XGBClassifier
+        xgb_param_grid = {
+            'learning_rate': [0.3],
+            'max_depth': [1],
+            'min_child_weight': [1],
+            'subsample': [0.7],
+            'colsample_bytree': [0.1],
+            'n_estimators': [100]
         }
         self.grid_search_classifier("rfc", RandomForestClassifier(class_weight='balanced', random_state=42, max_depth=3, min_samples_split=2), rfc_param_grid, X_train, y_train, X_test, y_test)
 
@@ -157,28 +203,28 @@ class Main():
 
         # MLPClassifier
         mlpc_param_grid = {
-            'hidden_layer_sizes': [(50,50,50)],
             'activation': ['relu'],
-            'solver': ['sgd'],
             'alpha': [0.00001],
+            'hidden_layer_sizes': [(50, 50, 50)],
             'learning_rate': ['constant'],
+            'solver': ['sgd']
         }
-        self.grid_search_classifier("mlpc", MLPClassifier(random_state=42, max_iter=100), mlpc_param_grid, X_train, y_train, X_test, y_test)
+        self.grid_search_classifier("mlpc", MLPClassifier(random_state=42, max_iter=10000), mlpc_param_grid, X_train, y_train, X_test, y_test)
 
         # AdaBoostClassifier
         ab_param_grid = {
-            'n_estimators': [150],
+            'algorithm': ['SAMME'],
             'learning_rate': [1.5],
-            'algorithm': ['SAMME']
+            'n_estimators': [150]
         }
         self.grid_search_classifier("ab", AdaBoostClassifier(random_state=42), ab_param_grid, X_train, y_train, X_test, y_test)
 
         # SGDClassifier
         sgdc_param_grid = {
-            'loss': ['modified_huber'],
             'alpha': [0.00004],
+            'eta0': [0.04],
             'learning_rate': ['constant'],
-            'eta0': [0.04]
+            'loss': ['modified_huber']
         }
         self.grid_search_classifier("sgdc", SGDClassifier(max_iter=1000, loss='modified_huber', random_state=42), sgdc_param_grid, X_train, y_train, X_test, y_test)
 
